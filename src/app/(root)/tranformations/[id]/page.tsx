ef30878f792853d4014c41ceaee7ce2398ceec93
Ann <ann.suparada@gmail.com>
2024-02-25 15:50:36 -0800

    setup routes, setup clerk auth

---
+++ src/app/(root)/tranformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TranformationPage = () => {
+  return <div>TranformationPage</div>
+}
+
+export default TranformationPage
---
+++ src/app/(root)/tranformations/add/[type]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AddTranformationPage = () => {
+  return <div>AddTranformationPage</div>
+}
+
+export default AddTranformationPage
