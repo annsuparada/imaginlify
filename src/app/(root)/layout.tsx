05df004a42a2a9d0eb05b6babc2757a83b6f557b
Ann <ann.suparada@gmail.com>
2024-02-26 14:56:14 -0800

    created sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from '@/components/shared/Sidebar';
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="root">
+    <main className="root">
+      <Sidebar />
+      
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
-    </div>
+    </main>
   )
 }
 
