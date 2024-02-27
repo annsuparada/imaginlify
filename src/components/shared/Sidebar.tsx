49b696a8ffc0ba47969fc27ab51df0ff5e94fae5
Ann <ann.suparada@gmail.com>
2024-02-26 16:33:57 -0800

    update sidebar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import { Button } from '../ui/button';
+import { Button } from '../ui/button'
 
 const Sidebar = () => {
   const pathname = usePathname()
@@ -21,11 +21,11 @@ const Sidebar = () => {
             height={28}
           />
         </Link>
-        
+
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidbar-nav_elements">
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname
 
                 return (
@@ -50,8 +50,36 @@ const Sidebar = () => {
                   </li>
                 )
               })}
+            </ul>
 
-              <li className="flex-center cursor-pointer gap-2 p-4">
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? 'bg-purple-gradient text-white'
+                        : 'text-gray-700'
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+
+              <li className="cursor-pointer gap-2 p-4">
                 <UserButton afterSignOutUrl="/" showName />
               </li>
             </ul>
